10c4a2e81a164ff3ec5d7eaca741bbc49d7f4b74
unknown <serhiiromanchuk@gmail.com>
2023-11-22 17:53:59 +0200

    Add sealed class Screen with routs for navigation

---
+++ app/src/main/java/com/serhiiromanchuk/utilitybills/presentation/navigation/Screen.kt
@@ -0,0 +1,24 @@
+package com.serhiiromanchuk.utilitybills.presentation.navigation
+
+sealed class Screen (
+    val route: String
+) {
+
+    data object MainScreen : Screen(ROUTE_MAIN_SCREEN)
+
+    data object InsertServiceScreen : Screen(ROUTE_INSERT_SERVICE_SCREEN)
+
+    data object BillsArchiveScreen : Screen(ROUTE_BILLS_ARCHIVE_SCREEN)
+
+    data object BillScreen : Screen(ROUTE_BILL_SCREEN)
+
+    data object RouteBillDetails : Screen(ROUTE_BILL_DETAILS)
+
+    companion object {
+        private const val ROUTE_MAIN_SCREEN = "main_screen"
+        private const val ROUTE_INSERT_SERVICE_SCREEN = "insert_service_screen"
+        private const val ROUTE_BILLS_ARCHIVE_SCREEN = "bills_archive_screen"
+        private const val ROUTE_BILL_SCREEN = "bill_screen"
+        private const val ROUTE_BILL_DETAILS = "bill_details_screen"
+    }
+}
